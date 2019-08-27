2bb9f334cb390c6cb2e64da607c03c443235169b
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-08-27 15:42:27 +0200

    Catch unknown errors to not expose them to the public

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -5,11 +5,18 @@ import logger from '../helpers/logger';
 
 // eslint-disable-next-line no-unused-vars
 export default function errorsMiddleware(err, req, res, next) {
-  // Check if error is public facing
-  if (!err.isPublic && process.env.NODE_ENV === 'production') {
+  // Check if error is public facing and known to us
+  if (
+    !(err instanceof APIError) ||
+    (!err.isPublic && process.env.NODE_ENV === 'production')
+  ) {
     // Log error message internally ..
-    const message = err.message || httpStatus[err.code];
-    logger.error(`${message} ${err.code} ${err.stack}`);
+    if (err.code) {
+      const message = err.message || httpStatus[err.code];
+      logger.error(`${message} ${err.code} ${err.stack}`);
+    } else {
+      logger.error(err.stack);
+    }
 
     // .. and expose generic message to public
     err = new APIError(httpStatus.INTERNAL_SERVER_ERROR);
