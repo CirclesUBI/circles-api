9a310bf00833c08345ab8599a633ffcb8a4abbc6
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-11-10 01:26:54 +0100

    Update dependencies and core

--- src/services/core.js
+++ src/services/core.js
@@ -1,4 +1,4 @@
-import CirclesCore from 'circles-core';
+import CirclesCore from '@circles/core';
 
 import web3 from './web3';
 
@@ -6,8 +6,10 @@ const core = new CirclesCore(web3, {
   safeMasterAddress: process.env.SAFE_ADDRESS,
   hubAddress: process.env.HUB_ADDRESS,
   proxyFactoryAddress: process.env.PROXY_FACTORY_ADDRESS,
+  graphNodeEndpoint: process.env.GRAPH_NODE_ENDPOINT,
   usernameServiceEndpoint: process.env.USERNAME_SERVICE_ENDPOINT,
   relayServiceEndpoint: process.env.RELAY_SERVICE_ENDPOINT,
+  subgraphName: process.env.SUBGRAPH_NAME,
 });
 
 export default core;
