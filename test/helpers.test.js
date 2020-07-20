ce39b6a467f4bc8f015d1dcbfc3dae8db2a648a7
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-07-20 12:07:23 +0200

    Add random test generator for helper method

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -9,6 +9,15 @@ describe('Helpers', () => {
       expect(minNumberString('5500', '5000')).toBe('5000');
       expect(minNumberString('5500000000', '70000000')).toBe('70000000');
       expect(minNumberString('20', '20')).toBe('20');
+
+      for (let i = 0; i < 1000; i += 1) {
+        const a = Math.floor(Math.random() * Math.MAX_SAFE_INTEGER);
+        const b = Math.floor(Math.random() * Math.MAX_SAFE_INTEGER);
+
+        expect(minNumberString(a.toString(), b.toString())).toBe(
+          Math.min(a, b).toString(),
+        );
+      }
     });
   });
 });
