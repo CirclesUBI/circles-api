1cd6df3d63479b80e0cec00bc695fb12a77c8f74
Andreas Dzialocha <x12@adz.garden>
2020-09-07 12:03:21 +0200

    Add uploads endpoint to resize and store avatar images on S3

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import httpStatus from 'http-status';
 
 import APIError from '../helpers/errors';
 import transfersRouter from './transfers';
+import uploadsRouter from './uploads';
 import usersRouter from './users';
 import { respondWithSuccess } from '../helpers/responses';
 
@@ -12,8 +13,9 @@ router.get('/', (req, res) => {
   respondWithSuccess(res);
 });
 
-router.use('/users', usersRouter);
 router.use('/transfers', transfersRouter);
+router.use('/uploads', uploadsRouter);
+router.use('/users', usersRouter);
 
 router.use(() => {
   throw new APIError(httpStatus.NOT_FOUND);
