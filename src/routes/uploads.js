04a7b2be7bd51f7c59ef8e2ff97e94294398df13
Andreas Dzialocha <x12@adz.garden>
2020-09-08 10:11:31 +0200

    Cleanup upload, rename image array field, handle error states better

--- src/routes/uploads.js
+++ src/routes/uploads.js
@@ -4,23 +4,26 @@ import uploadsController from '../controllers/uploads';
 import uploadFilesMiddleware from '../middlewares/uploads';
 import convertImagesMiddleware from '../middlewares/images';
 
+export const FIELD_NAME = 'files';
+
 const router = express.Router();
 
 router.post(
   '/avatar',
   uploadFilesMiddleware([
     {
-      name: 'avatar',
+      name: FIELD_NAME,
       maxCount: 1,
     },
   ]),
   convertImagesMiddleware([
     {
-      name: 'avatar',
+      name: FIELD_NAME,
       versions: [
         {
           width: 300,
           height: 300,
+          suffix: null,
         },
       ],
     },
