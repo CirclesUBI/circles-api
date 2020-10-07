c97bdf99a6d79f64571364f30686cf1f1eb5c0d6
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-10-07 14:41:22 +0200

    Remove paymentNote regex validation

--- src/validations/transfers.js
+++ src/validations/transfers.js
@@ -11,7 +11,6 @@ export default {
         from: customJoi.web3().address().addressChecksum().required(),
         paymentNote: Joi.string()
           .max(100)
-          .pattern(/^[\w\s!?:\-.,_*%@#&+)(]+$/)
           .empty(''),
         to: customJoi.web3().address().addressChecksum().required(),
         transactionHash: customJoi.web3().transactionHash().required(),
