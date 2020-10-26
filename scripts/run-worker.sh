6bf7e987157bbe2863588277f41f758a34ceeb65
Andreas Dzialocha <x12@adz.garden>
2020-10-26 11:13:21 +0100

    Migrate in wait-for-db.sh and introduce default variables

--- scripts/run-worker.sh
+++ scripts/run-worker.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-export NODE_ENV=production
-
 node ./build/worker.js
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-export NODE_ENV=production
-
-npm run db:migrate
-npm run db:seed
-
 node ./build/index.js
