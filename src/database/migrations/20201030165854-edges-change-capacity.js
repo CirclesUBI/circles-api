50c4429edd6e58fa81333fc52d5172a6adc00fbb
Andreas Dzialocha <x12@adz.garden>
2020-11-12 17:24:32 +0100

    Wrap migration into one transaction

--- src/database/migrations/20201030165854-edges-change-capacity.js
+++ src/database/migrations/20201030165854-edges-change-capacity.js
@@ -1,63 +1,113 @@
 const web3 = require('web3');
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    // Convert `capacity` field in `edges` table from number (in Ether) to
-    // String (in Wei)
-    const result = await queryInterface.sequelize.query(
-      'SELECT id, capacity FROM edges;',
-    );
-    const edges = result[1].rows;
+  up: async (queryInterface) => {
+    const transaction = await queryInterface.sequelize.transaction();
 
-    await queryInterface.addColumn('edges', 'capacity_tmp', {
-      type: Sequelize.STRING,
-    });
-
-    if (edges.length > 0) {
-      await Promise.all(
-        edges.map((edge) => {
-          return queryInterface.sequelize.query(
-            `UPDATE edges SET capacity_tmp = '${web3.utils.toWei(
-              edge.capacity.toString(),
-              'ether',
-            )}' WHERE id = ${edge.id};`,
-          );
-        }),
+    try {
+      // Convert `capacity` field in `edges` table from number (in Ether) to
+      // String (in Wei)
+      const result = await queryInterface.sequelize.query(
+        'SELECT id, capacity FROM edges;',
+        {
+          transaction,
+        },
       );
-    }
+      const edges = result[1].rows;
 
-    await queryInterface.removeColumn('edges', 'capacity');
-    await queryInterface.renameColumn('edges', 'capacity_tmp', 'capacity');
+      await queryInterface.sequelize.query(
+        'ALTER TABLE edges ADD COLUMN capacity_tmp VARCHAR(255);',
+        {
+          transaction,
+        },
+      );
+
+      if (edges.length > 0) {
+        await Promise.all(
+          edges.map((edge) => {
+            return queryInterface.sequelize.query(
+              `UPDATE edges SET capacity_tmp = '${web3.utils.toWei(
+                edge.capacity.toString(),
+                'ether',
+              )}' WHERE id = ${edge.id};`,
+              {
+                transaction,
+              },
+            );
+          }),
+        );
+      }
+
+      await queryInterface.sequelize.query(
+        'ALTER TABLE edges DROP COLUMN IF EXISTS capacity RESTRICT;',
+        {
+          transaction,
+        },
+      );
+      await queryInterface.sequelize.query(
+        'ALTER TABLE edges RENAME COLUMN capacity_tmp TO capacity;',
+        {
+          transaction,
+        },
+      );
+    } catch {
+      transaction.rollback();
+    }
   },
-  down: async (queryInterface, Sequelize) => {
-    // Convert `capacity` field in `edges` table from string (in Wei) to
-    // number (in ether)
-    const result = await queryInterface.sequelize.query(
-      'SELECT id, capacity FROM edges;',
-    );
+  down: async (queryInterface) => {
+    const transaction = await queryInterface.sequelize.transaction();
 
-    const edges = result[1].rows;
-
-    await queryInterface.addColumn('edges', 'capacity_tmp', {
-      type: Sequelize.INTEGER,
-    });
-
-    if (edges.length > 0) {
-      await Promise.all(
-        edges.map((edge) => {
-          return queryInterface.sequelize.query(
-            `UPDATE edges SET capacity_tmp = ${Math.floor(
-              parseFloat(
-                web3.utils.fromWei(edge.capacity.toString(), 'ether'),
-                10,
-              ),
-            )} WHERE id = ${edge.id};`,
-          );
-        }),
+    try {
+      // Convert `capacity` field in `edges` table from string (in Wei) to
+      // number (in ether)
+      const result = await queryInterface.sequelize.query(
+        'SELECT id, capacity FROM edges;',
+        {
+          transaction,
+        },
       );
-    }
 
-    await queryInterface.removeColumn('edges', 'capacity');
-    await queryInterface.renameColumn('edges', 'capacity_tmp', 'capacity');
+      const edges = result[1].rows;
+
+      await queryInterface.sequelize.query(
+        'ALTER TABLE edges ADD COLUMN capacity_tmp INTEGER;',
+        {
+          transaction,
+        },
+      );
+
+      if (edges.length > 0) {
+        await Promise.all(
+          edges.map((edge) => {
+            return queryInterface.sequelize.query(
+              `UPDATE edges SET capacity_tmp = ${Math.floor(
+                parseFloat(
+                  web3.utils.fromWei(edge.capacity.toString(), 'ether'),
+                  10,
+                ),
+              )} WHERE id = ${edge.id};`,
+              {
+                transaction,
+              },
+            );
+          }),
+        );
+      }
+
+      await queryInterface.sequelize.query(
+        'ALTER TABLE edges DROP COLUMN IF EXISTS capacity RESTRICT;',
+        {
+          transaction,
+        },
+      );
+      await queryInterface.sequelize.query(
+        'ALTER TABLE edges RENAME COLUMN capacity_tmp TO capacity;',
+        {
+          transaction,
+        },
+      );
+    } catch {
+      transaction.rollback();
+    }
   },
 };
