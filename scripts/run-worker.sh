98bcc9599f1445f0bc05b91ac58310b5067324b9
Andreas Dzialocha <x12@adz.garden>
2020-11-16 19:07:21 +0100

    Increase memory size to 4gb

--- scripts/run-worker.sh
+++ scripts/run-worker.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-node --max-old-space-size=2048 ./build/worker.js
+# Fix "JavaScript heap out of memory" error, see:
+# https://stackoverflow.com/questions/53230823/fatal-error-ineffective-mark-compacts-near-heap-limit-allocation-failed-javas
+export NODE_OPTIONS="--max-old-space-size=4096"
+
+node ./build/worker.js
