3e8d11247603cc0fd0045e0b37b875e592882000
ana0 <sarah.anne.friend@gmail.com>
2021-02-12 17:36:44 +0100

    Corrent rebase mistake in constants.js

--- src/constants.js
+++ src/constants.js
@@ -5,11 +5,6 @@ export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const PATHFINDER_BASE_PATH = path.join(__dirname, '..', 'pathfinder');
 export const EDGES_FILE_PATH = path.join(PATHFINDER_BASE_PATH, 'edges.json');
 
-export const EDGES_TMP_FILE_PATH = path.join(
-  PATHFINDER_BASE_PATH,
-  'edges.json-tmp',
-);
-
 export const PATHFINDER_FILE_PATH = path.join(
   PATHFINDER_BASE_PATH,
   'pathfinder',
