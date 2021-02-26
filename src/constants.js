f17b5bbbf30fda27ea936f7ee9756c02aca0a6a3
= <linne.louise@gmail.com>
2021-02-26 15:22:26 +0100

    Add test environment variable and using NODE_ENV in edges.json filename

--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,10 @@ import path from 'path';
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 export const PATHFINDER_BASE_PATH = path.join(__dirname, '..', 'pathfinder');
-export const EDGES_FILE_PATH = path.join(PATHFINDER_BASE_PATH, 'edges.json');
-
+export const EDGES_FILE_PATH = path.join(
+  PATHFINDER_BASE_PATH,
+  `edges-${process.env.NODE_ENV}.json`,
+);
 export const PATHFINDER_FILE_PATH = path.join(
   PATHFINDER_BASE_PATH,
   'pathfinder',
