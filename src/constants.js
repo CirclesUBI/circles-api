06309a36e98a921766f70aa7b99c0d0d85bc1446
Andreas Dzialocha <x12@adz.garden>
2021-03-11 14:39:27 +0100

    Move pathfinder binary into root, create edges-data folder in worker task

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,11 @@ import path from 'path';
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-export const PATHFINDER_BASE_PATH = path.join(__dirname, '..', 'pathfinder');
-export const EDGES_FILE_PATH = path.join(PATHFINDER_BASE_PATH, 'edges.json');
-
-export const PATHFINDER_FILE_PATH = path.join(
-  PATHFINDER_BASE_PATH,
-  'pathfinder',
+export const EDGES_DIRECTORY_NAME = 'edges-data';
+export const EDGES_FILE_PATH = path.join(
+  '..',
+  EDGES_DIRECTORY_NAME,
+  'edges.json',
 );
+
+export const PATHFINDER_FILE_PATH = path.join('..', 'pathfinder');
--- src/services/edgesFile.js
+++ src/services/edgesFile.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 import web3 from './web3';
-import { EDGES_FILE_PATH, PATHFINDER_BASE_PATH } from '../constants';
+import { EDGES_FILE_PATH, EDGES_DIRECTORY_NAME } from '../constants';
 
 const stringify = fastJsonStringify({
   title: 'Circles Edges Schema',
@@ -34,9 +34,15 @@ export async function writeToFile(
   tmpFileKey = web3.utils.randomHex(16).slice(2),
 ) {
   return new Promise((resolve, reject) => {
+    // Check if `edges-data` folder exists and create it otherwise
+    const edgesDataPath = path.join('..', EDGES_DIRECTORY_NAME);
+    if (!fs.existsSync(edgesDataPath)) {
+      fs.mkdirSync(edgesDataPath);
+    }
+
     // Write to temporary file first
     const tmpFilePath = path.join(
-      PATHFINDER_BASE_PATH,
+      edgesDataPath,
       `edges.json-tmp-${tmpFileKey}`,
     );
 
