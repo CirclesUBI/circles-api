8d586c067fa9e18dc28b5d9161c97e0c2c61f57e
Andreas Dzialocha <x12@adz.garden>
2021-04-28 17:21:34 +0200

    Add more tests and fix cases

--- src/helpers/reduce.js
+++ src/helpers/reduce.js
@@ -6,7 +6,7 @@ export default function reduceCapacities(
 ) {
   return edges
     .filter((edge) => {
-      return edge.capacity.length > bufferDecimals + 1;
+      return edge.capacity.length >= bufferDecimals + 1;
     })
     .reduce((acc, edge) => {
       acc.push({
@@ -22,7 +22,7 @@ export function reduceCapacity(
   bufferDecimals = DEFAULT_BUFFER_DECIMALS,
 ) {
   // Ignore too small values
-  if (value.length <= bufferDecimals + 1) {
+  if (value.length < bufferDecimals + 1) {
     return value;
   }
 
@@ -32,8 +32,8 @@ export function reduceCapacity(
   const figureToChange = parseInt(value[index], 10);
 
   if (figureToChange === 0) {
-    const reducedFrontNumber = parseInt(frontNumber, 10) - 1;
-    return `${reducedFrontNumber}0${endOfNumber}`;
+    const reduced = parseInt(frontNumber, 10) - 1;
+    return `${reduced === 0 ? '' : reduced}9${endOfNumber}`;
   } else {
     return `${frontNumber}${figureToChange - 1}${endOfNumber}`;
   }
