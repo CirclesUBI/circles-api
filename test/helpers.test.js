71064955d7bc384be1f52b320ca5788e923ba5a6
Louise Linné <linne.louise@gmail.com>
2021-04-29 13:09:28 +0200

    Fix errors in tests for reduceCapacities

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -93,70 +93,67 @@ describe('Helpers', () => {
 
   describe('reduceCapacity', () => {
     it('should reduce capacity values correctly', () => {
-      // Alias for better readability
-      const r = reduceCapacity;
+      expect(reduceCapacity('10000', 1)).toBe('9999');
+      expect(reduceCapacity('10000', 2)).toBe('9990');
+      expect(reduceCapacity('10000', 3)).toBe('9900');
+      expect(reduceCapacity('10000', 4)).toBe('9000');
 
-      expect(r('10000', 1)).toBe('9999');
-      expect(r('10000', 2)).toBe('9990');
-      expect(r('10000', 3)).toBe('9900');
-      expect(r('10000', 4)).toBe('9000');
+      expect(reduceCapacity('12345', 1)).toBe('12344');
+      expect(reduceCapacity('12345', 2)).toBe('12335');
+      expect(reduceCapacity('12345', 3)).toBe('12245');
+      expect(reduceCapacity('12345', 4)).toBe('11345');
 
-      expect(r('12345', 1)).toBe('12344');
-      expect(r('12345', 2)).toBe('12335');
-      expect(r('12345', 3)).toBe('12245');
-      expect(r('12345', 4)).toBe('11345');
+      expect(reduceCapacity('17000000000000000', 1)).toBe('16999999999999999');
+      expect(reduceCapacity('17000000000000000', 2)).toBe('16999999999999990');
+      expect(reduceCapacity('17000000000000000', 3)).toBe('16999999999999900');
+      expect(reduceCapacity('17000000000000000', 4)).toBe('16999999999999000');
+      expect(reduceCapacity('17000000000000000', 5)).toBe('16999999999990000');
+      expect(reduceCapacity('17000000000000000', 6)).toBe('16999999999900000');
+      expect(reduceCapacity('17000000000000000', 7)).toBe('16999999999000000');
+      expect(reduceCapacity('17000000000000000', 8)).toBe('16999999990000000');
+      expect(reduceCapacity('17000000000000000', 9)).toBe('16999999900000000');
+      expect(reduceCapacity('17000000000000000', 10)).toBe('16999999000000000');
+      expect(reduceCapacity('17000000000000000', 11)).toBe('16999990000000000');
+      expect(reduceCapacity('17000000000000000', 12)).toBe('16999900000000000');
+      expect(reduceCapacity('17000000000000000', 13)).toBe('16999000000000000');
+      expect(reduceCapacity('17000000000000000', 14)).toBe('16990000000000000');
+      expect(reduceCapacity('17000000000000000', 15)).toBe('16900000000000000');
+      expect(reduceCapacity('17000000000000000', 16)).toBe('16000000000000000');
 
-      expect(r('17000000000000000', 1)).toBe('16999999999999999');
-      expect(r('17000000000000000', 2)).toBe('16999999999999990');
-      expect(r('17000000000000000', 3)).toBe('16999999999999900');
-      expect(r('17000000000000000', 4)).toBe('16999999999999000');
-      expect(r('17000000000000000', 5)).toBe('16999999999990000');
-      expect(r('17000000000000000', 6)).toBe('16999999999900000');
-      expect(r('17000000000000000', 7)).toBe('16999999999000000');
-      expect(r('17000000000000000', 8)).toBe('16999999990000000');
-      expect(r('17000000000000000', 9)).toBe('16999999900000000');
-      expect(r('17000000000000000', 10)).toBe('16999999000000000');
-      expect(r('17000000000000000', 11)).toBe('16999990000000000');
-      expect(r('17000000000000000', 12)).toBe('16999900000000000');
-      expect(r('17000000000000000', 13)).toBe('16999000000000000');
-      expect(r('17000000000000000', 14)).toBe('16990000000000000');
-      expect(r('17000000000000000', 15)).toBe('16900000000000000');
-      expect(r('17000000000000000', 16)).toBe('16000000000000000');
-
-      expect(r('1094000000000012345', 1)).toBe('1094000000000012344');
-      expect(r('1094000000000012345', 2)).toBe('1094000000000012335');
-      expect(r('1094000000000012345', 3)).toBe('1094000000000012245');
-      expect(r('1094000000000012345', 4)).toBe('1094000000000011345');
-      expect(r('1094000000000012345', 5)).toBe('1094000000000002345');
-      expect(r('1094000000000012345', 6)).toBe('1093999999999912345');
-      expect(r('1094000000000012345', 7)).toBe('1093999999999012345');
-      expect(r('1094000000000012345', 8)).toBe('1093999999990012345');
-      expect(r('1094000000000012345', 9)).toBe('1093999999900012345');
-      expect(r('1094000000000012345', 10)).toBe('1093999999000012345');
-      expect(r('1094000000000012345', 11)).toBe('1093999990000012345');
-      expect(r('1094000000000012345', 12)).toBe('1093999900000012345');
-      expect(r('1094000000000012345', 13)).toBe('1093999000000012345');
-      expect(r('1094000000000012345', 14)).toBe('1093990000000012345');
-      expect(r('1094000000000012345', 15)).toBe('1093900000000012345');
-      expect(r('1094000000000012345', 16)).toBe('1093000000000012345');
-      expect(r('1094000000000012345', 17)).toBe('1084000000000012345');
-      expect(r('1094000000000012345', 18)).toBe('994000000000012345');
+      expect(reduceCapacity('1094000000000012345', 1)).toBe('1094000000000012344');
+      expect(reduceCapacity('1094000000000012345', 2)).toBe('1094000000000012335');
+      expect(reduceCapacity('1094000000000012345', 3)).toBe('1094000000000012245');
+      expect(reduceCapacity('1094000000000012345', 4)).toBe('1094000000000011345');
+      expect(reduceCapacity('1094000000000012345', 5)).toBe('1094000000000002345');
+      expect(reduceCapacity('1094000000000012345', 6)).toBe('1093999999999912345');
+      expect(reduceCapacity('1094000000000012345', 7)).toBe('1093999999999012345');
+      expect(reduceCapacity('1094000000000012345', 8)).toBe('1093999999990012345');
+      expect(reduceCapacity('1094000000000012345', 9)).toBe('1093999999900012345');
+      expect(reduceCapacity('1094000000000012345', 10)).toBe('1093999999000012345');
+      expect(reduceCapacity('1094000000000012345', 11)).toBe('1093999990000012345');
+      expect(reduceCapacity('1094000000000012345', 12)).toBe('1093999900000012345');
+      expect(reduceCapacity('1094000000000012345', 13)).toBe('1093999000000012345');
+      expect(reduceCapacity('1094000000000012345', 14)).toBe('1093990000000012345');
+      expect(reduceCapacity('1094000000000012345', 15)).toBe('1093900000000012345');
+      expect(reduceCapacity('1094000000000012345', 16)).toBe('1093000000000012345');
+      expect(reduceCapacity('1094000000000012345', 17)).toBe('1084000000000012345');
+      expect(reduceCapacity('1094000000000012345', 18)).toBe('994000000000012345');
 
       // Using default value
-      expect(r('17000000000000000')).toBe('16900000000000000');
-      expect(r('91000000000000000')).toBe('90900000000000000');
-      expect(r('9999999999999999999')).toBe('9899999999999999999');
+      expect(reduceCapacity('17000000000000000')).toBe('16900000000000000');
+      expect(reduceCapacity('91000000000000000')).toBe('90900000000000000');
+      expect(reduceCapacity('9999999999999999999')).toBe('9999899999999999999');
     });
 
     it('should not reduce capacity when the value is the same order of magnitude as the the buffer', () => {
-      expect(r('10000', 5)).toBe('10000');
-      expect(r('12345', 5)).toBe('12345');
-      expect(r('11000000000000000', 17)).toBe('11000000000000000');
+      expect(reduceCapacity('10000', 5)).toBe('10000');
+      expect(reduceCapacity('12345', 5)).toBe('12345');
+      expect(reduceCapacity('11000000000000000', 17)).toBe('11000000000000000');
     });
 
     it('should not reduce capacity when the value is smaller than the buffer', () => {
-      expect(r('1000', 15)).toBe('1000');
-      expect(r('123', 4)).toBe('123');
+      expect(reduceCapacity('1000', 15)).toBe('1000');
+      expect(reduceCapacity('123', 4)).toBe('123');
     });
   });
 });
