71ed0da759edd78cd09fd43577a09f8698d6e2d6
llunaCreixent <elesmp@protonmail.com>
2022-01-11 16:57:42 +0100

    Check transfer steps before returning them

--- src/helpers/loop.js
+++ src/helpers/loop.js
@@ -1,5 +1,11 @@
 const LOOP_INTERVAL = 5000;
 const MAX_ATTEMPTS = 12;
+// Times the method will repeat the request after an error or condition failure
+const RETRIES_ON_FAIL_DEFAULT = 3;
+// When a request fails wait a few ms before we do it again
+const WAIT_AFTER_FAIL_DEFAULT = 5000;
+// Error message used to indicate failed condition check
+const TRIED_TOO_MANY_TIMES = 'Tried too many times waiting for condition.';
 
 export default async function loop(request, condition) {
   return new Promise((resolve, reject) => {
@@ -22,4 +28,52 @@ export default async function loop(request, condition) {
       }
     }, LOOP_INTERVAL);
   });
+}
+
+// This helper method repeats calling a request when it fails or when a
+// condition was not reached after some attempts.
+//
+// Use this method if you want to make a crucial request for creating or
+// updating data somewhere. When this request fails, for example because of
+// networking issues or server outage, this helper method will try to repeat
+// the request for you until it succeeded.
+export async function waitAndRetryOnFail(
+  requestFn,
+  loopFn,
+  {
+    maxAttemptsOnFail = RETRIES_ON_FAIL_DEFAULT,
+    waitAfterFail = WAIT_AFTER_FAIL_DEFAULT,
+  } = {},
+) {
+  // Count all attempts to retry when something failed
+  let attempt = 1;
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    try {
+      // Make request and wait for response
+      const response = await requestFn();
+
+      // Wait for a few seconds until our condition arrives
+      await loopFn();
+
+      // Finish when request was successful and condition arrived!
+      return response;
+    } catch (error) {
+      // Something went wrong, either the condition did not arrive or the
+      // request failed
+      if (attempt >= maxAttemptsOnFail) {
+        // We tried too often, propagate error and stop here
+        throw error;
+      }
+
+      // Wait when request failed to prevent calling the request too fast again
+      if (error.message !== TRIED_TOO_MANY_TIMES) {
+        await wait(waitAfterFail);
+      }
+
+      // Lets try again ..
+      attempt += 1;
+    }
+  }
 }
--- src/services/findTransferSteps.js
+++ src/services/findTransferSteps.js
@@ -1,9 +1,61 @@
+import HubContract from 'circles-contracts/build/contracts/Hub.json';
+import TokenContract from 'circles-contracts/build/contracts/Token.json';
 import findTransferSteps from '@circles/transfer';
 import { performance } from 'perf_hooks';
+
+import web3 from './web3';
+import tasks from '../tasks';
+import submitJob from '../tasks/submitJob';
+import waitAndRetryOnFail from '../helpers/loop';
+import EdgeUpdateManager from './edgesUpdate';
 
 import { EDGES_FILE_PATH, PATHFINDER_FILE_PATH } from '../constants';
 
 const DEFAULT_PROCESS_TIMEOUT = 1000 * 60;
+
+const hubContract = new web3.eth.Contract(
+  HubContract.abi,
+  process.env.HUB_ADDRESS,
+);
+
+async function isStepValid(tokenAddress, tokenOwner, sender, receiver, plannedLimit) {
+  // Get send limit
+  const limit = await hubContract.methods.checkSendLimit(tokenOwner, sender, receiver).call();
+
+  // Get Token balance
+  const tokenContract = new web3.eth.Contract(
+    TokenContract.abi,
+    tokenAddress,
+  );
+  const balance = await tokenContract.methods.balanceOf(sender).call();
+
+  // The capacity is the minimum between the sendLimit and the balance
+  const capacity = minNumberString(limit, balance);
+  return isValid = web3.utils.toBN(plannedLimit).lte(web3.utils.toBN(capacity));
+}
+
+// Returns true if the transfer steps are valid
+// Returns false and updates the steps if they are invalid
+async function checkTransferSteps(transferSteps){
+  const edgeUpdateManager = new EdgeUpdateManager();
+  let allStepsValid = true;
+  for (let step of transferSteps) {
+    const tokenAddress = await hubContract.methods.userToToken(step.tokenOwnerAddress).call();
+    const isValid = await isStepValid(tokenAddress, step.tokenOwnerAddress, step.from, step.to, step.value);
+    if (!isValid) {
+      allStepsValid = false;
+      // Update the edge
+      await edgeUpdateManager.updateEdge(
+        {
+          token: step.tokenOwnerAddress,
+          from: step.from,
+          to: step.to,
+        },
+        tokenAddress,
+      );
+    }
+  }
+}
 
 export default async function transferSteps({ from, to, value }) {
   if (from === to) {
@@ -16,18 +68,40 @@ export default async function transferSteps({ from, to, value }) {
     ? parseInt(process.env.TRANSFER_STEPS_TIMEOUT, 10)
     : DEFAULT_PROCESS_TIMEOUT;
 
-  const result = await findTransferSteps(
-    {
-      from,
-      to,
-      value,
+  await waitAndRetryOnFail(
+    async () => {
+      return await await findTransferSteps(
+        {
+          from,
+          to,
+          value,
+        },
+        {
+          edgesFile: EDGES_FILE_PATH,
+          pathfinderExecutable: PATHFINDER_FILE_PATH,
+          timeout,
+        },
+      );
+    },
+    async (result) => {
+      // Checks that transfer steps are valid
+      const validTransferSteps = await checkTransferSteps(result.transferSteps)
+      if( !validTransferSteps ){
+        // export edges.json
+        submitJob(tasks.exportEdges, 'exportEdges-before-transfer');
+        // findTransferSteps again
+        console.log("*************** Return false");
+        return false;
+      }
+      console.log("*************** Return true");
+      return true;
     },
     {
-      edgesFile: EDGES_FILE_PATH,
-      pathfinderExecutable: PATHFINDER_FILE_PATH,
-      timeout,
-    },
+      maxAttemptsOnFail: 2,
+      waitAfterFail: 4000,
+    }
   );
+
 
   const endTime = performance.now();
 
