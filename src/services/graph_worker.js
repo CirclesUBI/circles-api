e6ca96fe9dcb364b9b50262a28e7b6c485aad154
jacque <jacque@jacques-mbp.intern>
2022-05-16 17:56:13 +0200

    fix: updated graph_worker

--- src/services/graph_worker.js
+++ src/services/graph_worker.js
@@ -1,18 +1,27 @@
-// run every 3 second - monitoring check rules
-// when the rules are met it changes CURRENT_GRAPH_NODE_ENDPOINT
-// when this change happens write to CURRENT_GRAPH_NODE_ENDPOINT
+import graph from './graph';
+import logger from '../helpers/logger';
 
-//import { currentEndpoint } from './test_graph';
-// const mainEndpoint = process.env.GRAPH_NODE_ENDPOINT_MAIN;
-// const backupEndpoint = process.env.GRAPH_NODE_ENDPOINT_BACKUP;
-
-//import { rule1 } from './test_graph';
-const ex = require('./test_graph');
-ex.rule1()
-  .then((endpoint) => {
-    const currentEndpoint = endpoint;
-    console.log('CURR', currentEndpoint);
+var currentEndpoint = graph.getCurrentEndpoint();
+graph
+  .differenceRule(currentEndpoint)
+  .then((newEndpoint) => {
+    if (typeof newEndpoint !== 'undefined') {
+      graph.setCurrentEndpoint(newEndpoint, './config.json');
+      logger.info('Grahnode endpoint changed to: ', newEndpoint);
+    }
   })
   .catch(() => {
-    console.log('Unable to run rule 1');
+    logger.info('Unable to difference rule');
+  });
+
+graph
+  .healthRule(currentEndpoint)
+  .then((newEndpoint) => {
+    if (typeof newEndpoint !== 'undefined') {
+      graph.setCurrentEndpoint(newEndpoint, './config.json');
+      logger.info('Grahnode endpoint changed to: ', newEndpoint);
+    }
+  })
+  .catch(() => {
+    logger.error('Unable to difference rule');
   });
