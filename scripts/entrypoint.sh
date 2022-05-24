cd2c95351f562d27a18ec020d8518104bed58f1c
jacque <jacque@jacques-MBP.fritz.box>
2022-05-24 12:35:19 +0200

    make @ a string

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,11 +7,12 @@ CMD="$@"
 # Set default values
 POSTGRES_PORT=${POSTGRES_PORT:-5432}
 NODE_ENV=${NODE_ENV:-"production"}
+SIM="@"
 # Wait until database is ready
 ./wait-for-it.sh "$POSTGRES_HOST:$POSTGRES_PORT" -t 60
 
 # Set DATABASE_URL env variable in correct format for application
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DATABASE_API
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD$SYM$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DATABASE_API
 
 # Run migrations
 npm run db:migrate
