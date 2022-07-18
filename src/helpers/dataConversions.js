720355323b6c10bbcb7dc6308765d19aac1558da
jacque <jacque@jacques-MBP.fritz.box>
2022-07-18 17:08:40 +0200

    dataconversio no needed anymore

--- src/helpers/dataConversions.js
+++
@@ -1,8 +0,0 @@
-export default function arrayToCSV(data) {
-  return data
-    .map(function (line) {
-      return JSON.stringify(Object.values(line));
-    })
-    .join('\n')
-    .replace(/(^\[)|(\]$)|(")/gm, '');
-}
