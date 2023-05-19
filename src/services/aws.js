25a95b116ad2c6728a43ee52b07e5f33596988bb
Elena San Miguel <elesmp@protonmail.com>
2023-05-19 09:28:45 +0200

    Update to Node16 and update dependencies (#178)
    * nvm use 16; npm i
    
    * ncu -u -t patch
    
    * ncu -u -t minor
    
    * Migrate code to use AWS SDK for JavaScript (v3)
    
    * Add node engine in package.json

--- src/services/aws.js
+++ src/services/aws.js
@@ -1,5 +1,7 @@
 import aws from 'aws-sdk';
 
+import { S3 } from "@aws-sdk/client-s3";
+
 aws.config.region = process.env.AWS_REGION || 'eu-central-1';
 
-export const s3 = new aws.S3();
+export const s3 = new S3();
