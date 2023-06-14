969eadde556f41f04a59c2be5853a1397c8c02e1
llunaCreixent <elesmp@protonmail.com>
2023-06-14 11:40:07 +0200

    Update dependencies: @circles/core

--- src/services/core.js
+++ src/services/core.js
@@ -4,13 +4,14 @@ import web3 from './web3';
 
 const core = new CirclesCore(web3, {
   apiServiceEndpoint: process.env.API_SERVICE_ENDPOINT,
+  fallbackHandlerAddress: process.env.SAFE_DEFAULT_CALLBACK_HANDLER,
   graphNodeEndpoint: process.env.GRAPH_NODE_ENDPOINT,
   hubAddress: process.env.HUB_ADDRESS,
+  pathfinderServiceEndpoint: process.env.PATHFINDER_SERVICE_ENDPOINT,
   proxyFactoryAddress: process.env.PROXY_FACTORY_ADDRESS,
   relayServiceEndpoint: process.env.RELAY_SERVICE_ENDPOINT,
   safeMasterAddress: process.env.SAFE_ADDRESS,
   subgraphName: process.env.SUBGRAPH_NAME,
-  fallbackHandlerAddress: process.env.SAFE_DEFAULT_CALLBACK_HANDLER,
 });
 
 export default core;
