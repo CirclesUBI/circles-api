eb43e372f946a7c8a9f3d334e16b3e1a5edd6a08
JacqueGM <jacquemate@riseup.net>
2023-08-14 16:42:30 +0200

    remove console.log

--- test/news-find.test.js
+++ test/news-find.test.js
@@ -66,10 +66,6 @@ describe('GET /news/?afterDate=... - Search via date', () => {
       .set('Accept', 'application/json')
       .expect(httpStatus.OK)
       .expect(({ body }) => {
-        console.log(body);
-        console.log(news);
-        console.log(news[2]);
-        console.log(body.data);
         if (
           body.data.length !== 2 ||
           body.data[0].message.en !== news[4].message_en ||
