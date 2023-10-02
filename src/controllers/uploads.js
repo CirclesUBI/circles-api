89e5d399bfe1d2f00f78f3fe517ccdd6c63df687
Elena San Miguel <elesmp@protonmail.com>
2023-10-02 13:18:58 +0200

    Add delete Avatar Image endpoint (#209)

--- src/controllers/uploads.js
+++ src/controllers/uploads.js
@@ -1,11 +1,14 @@
-import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
 import httpStatus from 'http-status';
 import mime from 'mime';
 
 import APIError from '../helpers/errors';
+import User from '../models/users';
 import { FIELD_NAME } from '../routes/uploads';
-import { respondWithSuccess } from '../helpers/responses';
-import { s3 } from '../services/aws';
+import { respondWithSuccess, respondWithError } from '../helpers/responses';
+import { s3, AWS_S3_DOMAIN } from '../services/aws';
+
+export const KEY_PATH = 'uploads/avatars/';
 
 export default {
   uploadAvatarImage: async (req, res, next) => {
@@ -23,7 +26,7 @@ export default {
       }
 
       const { buffer, fileName, fileType } = req.locals.images[FIELD_NAME][0];
-      const key = `uploads/avatars/${fileName}`;
+      const key = `${KEY_PATH}${fileName}`;
 
       const params = {
         Bucket: bucket, // The name of the bucket.
@@ -37,12 +40,48 @@ export default {
       respondWithSuccess(
         res,
         {
-          url: `https://${bucket}.s3.amazonaws.com/${key}`,
+          url: `https://${bucket}.${AWS_S3_DOMAIN}/${key}`,
           fileName,
           fileType,
         },
         results.$metadata.httpStatusCode,
       );
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  deleteAvatarImage: async (req, res, next) => {
+    const bucket = process.env.AWS_S3_BUCKET;
+    try {
+      const { url } = req.body;
+      const imageUrl = new URL(url);
+      if (
+        imageUrl.host != `${bucket}.${AWS_S3_DOMAIN}` ||
+        !imageUrl.pathname.startsWith(`/${KEY_PATH}`)
+      ) {
+        respondWithError(res, {}, httpStatus.BAD_REQUEST);
+      } else {
+        // Check that the url is not used in the users db
+        const avatarExists = await User.findOne({
+          where: {
+            avatarUrl: url,
+          },
+        });
+        if (avatarExists) {
+          respondWithError(res, {}, httpStatus.UNPROCESSABLE_ENTITY);
+        } else {
+          const pathSplit = imageUrl.pathname.split('/');
+          const fileName = pathSplit[pathSplit.length - 1];
+          const key = `${KEY_PATH}${fileName}`;
+          const params = {
+            Bucket: bucket, // The name of the bucket.
+            Key: key, // The name of the object.
+          };
+          const results = await s3.send(new DeleteObjectCommand(params));
+          respondWithSuccess(res, {}, results.$metadata.httpStatusCode);
+        }
+      }
     } catch (error) {
       next(error);
     }
--- src/services/aws.js
+++ src/services/aws.js
@@ -1,6 +1,9 @@
 import { S3 } from '@aws-sdk/client-s3';
+
 const REGION = process.env.AWS_REGION || 'eu-central-1';
 
 const s3 = new S3({ region: REGION });
+
+export const AWS_S3_DOMAIN = 's3.amazonaws.com';
 
 export { s3 };
