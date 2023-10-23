9d870dc6dcdccf05ec3ba9b5295ae8401b1a82d3
JacqueGM <jacquemate@riseup.net>
2023-10-23 11:56:19 +0200

    update core to .env

--- test/utils/core.js
+++ test/utils/core.js
@@ -1,17 +1,19 @@
 import CirclesCore from '@circles/core';
 import ethProvider from './ethProvider';
+
 const core = new CirclesCore(ethProvider, {
-  hubAddress: '0xCfEB869F69431e42cdB54A4F4f105C19C080A601',
-  apiServiceEndpoint: 'http://api.circles.land',
-  fallbackHandlerAddress: '0x67B5656d60a809915323Bf2C40A8bEF15A152e3e',
-  graphNodeEndpoint: 'http://graph.circles.lan',
-  pathfinderServiceEndpoint: 'ATHFINDER_SERVICE_ENDPOINT',
-  pathfinderType: 'cli',
-  proxyFactoryAddress: '0x9b1f7F645351AF3631a656421eD2e40f2802E6c0',
-  relayServiceEndpoint: 'http://localhost:3002',
-  safeMasterAddress: '0x2612Af3A521c2df9EAF28422Ca335b04AdF3ac66',
-  subgraphName: 'circlesubi/circles-subgraph',
-  multiSendAddress: '0xe982E462b094850F12AF94d21D470e21bE9D0E9C',
-  multiSendCallOnlyAddress: '0x0290FB167208Af455bB137780163b7B7a9a10C16',
+  hubAddress: process.env.HUB_ADDRESS,
+  apiServiceEndpoint: process.env.API_SERVICE_ENDPOINT,
+  fallbackHandlerAddress: process.env.SAFE_DEFAULT_CALLBACK_HANDLER,
+  graphNodeEndpoint: process.env.GRAPH_NODE_ENDPOINT,
+  pathfinderServiceEndpoint: process.env.PATHFINDER_SERVICE_ENDPOINT,
+  pathfinderType: process.env.PATHFINDER_TYPE,
+  proxyFactoryAddress: process.env.PROXY_FACTORY_ADDRESS,
+  relayServiceEndpoint: process.env.RELAY_SERVICE_ENDPOINT,
+  safeMasterAddress: process.env.SAFE_ADDRESS,
+  subgraphName: process.env.SUBGRAPH_NAME,
+  multiSendAddress: process.env.MULTI_SEND_ADDRESS,
+  multiSendCallOnlyAddress: process.env.MULTI_SEND_CALL_ONLY_ADDRESS,
 });
+
 export default core;
