512fbf4d1af6218206f6e53b198410684281870a
llunaCreixent <elesmp@protonmail.com>
2024-10-01 10:58:15 +0200

    use new s3 bucket

--- src/services/aws.js
+++ src/services/aws.js
@@ -4,6 +4,6 @@ const REGION = process.env.AWS_REGION || 'eu-central-1';
 
 const s3 = new S3({ region: REGION });
 
-export const AWS_S3_DOMAIN = 's3.amazonaws.com';
+export const AWS_S3_DOMAIN = 'fra1.cdn.digitaloceanspaces.com';
 
 export { s3 };
