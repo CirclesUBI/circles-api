b76e239052fcdbb6ef393a4faaf3fb3450f3f596
llunaCreixent <elesmp@protonmail.com>
2024-10-03 12:45:55 +0200

    add param for aws config

--- src/services/aws.js
+++ src/services/aws.js
@@ -3,12 +3,13 @@ import { S3 } from '@aws-sdk/client-s3';
 const REGION = process.env.AWS_REGION || 'fra1';
 
 const s3 = new S3({
+  forcePathStyle: false, // Configures to use subdomain/virtual calling format.
   region: REGION,
   endpoint: 'https://fra1.cdn.digitaloceanspaces.com',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 export const AWS_S3_DOMAIN = 'fra1.cdn.digitaloceanspaces.com';
