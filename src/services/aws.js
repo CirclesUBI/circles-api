c286536e7fb3021346b5d31ff44a958758b83854
llunaCreixent <elesmp@protonmail.com>
2024-10-03 14:23:44 +0200

    do not use cdn

--- src/services/aws.js
+++ src/services/aws.js
@@ -5,7 +5,7 @@ const REGION = process.env.AWS_REGION || 'fra1';
 const s3 = new S3({
   forcePathStyle: false, // Configures to use subdomain/virtual calling format.
   region: REGION,
-  endpoint: 'https://fra1.cdn.digitaloceanspaces.com',
+  endpoint: 'https://fra1.digitaloceanspaces.com',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
