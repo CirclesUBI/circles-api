a3ac58e8270b6368232d2634c5716c2f35888fce
llunaCreixent <elesmp@protonmail.com>
2024-10-03 12:08:18 +0200

    set credentials param for aws

--- src/services/aws.js
+++ src/services/aws.js
@@ -5,6 +5,10 @@ const REGION = process.env.AWS_REGION || 'fra1';
 const s3 = new S3({
   region: REGION,
   endpoint: 'https://fra1.cdn.digitaloceanspaces.com',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
 });
 
 export const AWS_S3_DOMAIN = 'fra1.cdn.digitaloceanspaces.com';
