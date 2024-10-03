d41853edccbde8b071a1fd5f1ceaa855701f0132
llunaCreixent <elesmp@protonmail.com>
2024-10-03 11:18:54 +0200

    update region

--- src/services/aws.js
+++ src/services/aws.js
@@ -1,6 +1,6 @@
 import { S3 } from '@aws-sdk/client-s3';
 
-const REGION = process.env.AWS_REGION || 'eu-central-1';
+const REGION = process.env.AWS_REGION || 'fra1';
 
 const s3 = new S3({ region: REGION });
 
