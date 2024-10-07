7b168a0583fb56e3f30adefeac758ab60dbc34b4
Elena San Miguel <elesmp@protonmail.com>
2024-10-07 13:55:20 +0200

    use new s3 bucket (#225)
    * use new s3 bucket
    * update region
    * add endpoint
    * set credentials param for aws
    * add param for aws config
    * update aws-sdk

--- src/services/aws.js
+++ src/services/aws.js
@@ -1,9 +1,17 @@
 import { S3 } from '@aws-sdk/client-s3';
 
-const REGION = process.env.AWS_REGION || 'eu-central-1';
+const REGION = process.env.AWS_REGION || 'fra1';
 
-const s3 = new S3({ region: REGION });
+const s3 = new S3({
+  forcePathStyle: false, // Configures to use subdomain/virtual calling format.
+  region: REGION,
+  endpoint: 'https://fra1.digitaloceanspaces.com',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
-export const AWS_S3_DOMAIN = 's3.amazonaws.com';
+export const AWS_S3_DOMAIN = 'fra1.cdn.digitaloceanspaces.com';
 
 export { s3 };
